d8637dc37a8eca2119f987f98e6a8de30883a382
guagua777 <395968255@qq.com>
2026-05-22 16:24:19 +0800

    commit

---
+++ add/adder/src/adder_sub/mod.rs
@@ -0,0 +1,5 @@
+
+
+pub fn adder_sub(left: u64, right: u64) -> u64 {
+    left - right
+}
--- add/adder/src/main.rs
+++ add/adder/src/main.rs
@@ -1,7 +1,17 @@
 // use add_one;
 
+// 声明模块
+mod adder_sub;
+// 使用模块
+use adder_sub::adder_sub;
 
 fn main() {
     let num = 10;
     println!("Hello, world! {num} plus one is {}!", add_one::add_one(num));
+
+    // 打印当前的路径
+    println!("Current path: {:?}", std::env::current_dir());
 }
+
+
+// 模块也是先声明，后使用
